496e72d7a0b068484b871f22f3493e2a4d70b727
Peter Suggate <petersuggate@gmail.com>
2020-08-06 20:19:14 +1200

    Disable unnecessary eslint rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-empty-function": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     // "comma-dangle": "off",
   },
   overrides: [
